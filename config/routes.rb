9529c58596afe5b109d30af77b8f01dd0823a42e
lokeshh <lokeshhsharma@gmail.com>
2016-06-19 20:09:51 +0000

    remove edit and delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :codes
+  resources :codes, except: [:edit, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
